c398bfafaa81aa370eed47be1fdafe9b31cf4913
devinrankin <devinrankin@proton.me>
2025-08-23 17:01:25 -0400

    working on bank switching mechanism

--- src/gbc/cartridge.h
+++ src/gbc/cartridge.h
@@ -1,6 +1,6 @@
 #ifndef GBC_EMU_CART_H_
 #define GBC_EMU_CART_H_
-#include "memory.h"
+#include <cstddef>
 #include <cstdint>
 
 static constexpr unsigned int HEADER_ENTRYPOINT = 0x0100;
@@ -10,12 +10,18 @@ class Cartridge {
         Cartridge();
         ~Cartridge();
         bool load_rom(const char* filepath);
-        uint8_t* rom_data;
-        unsigned int rom_size;
-        unsigned int ram_size;
-        uint16_t cart_type;
+        uint8_t read_rom(uint16_t address);
+        void write_rom(uint16_t address, uint8_t value);
     private:
-        MBC_TYPE eval_cart_type(uint8_t value);
         bool verify_header_checksum();
+        void eval_cart_type(uint8_t value);
+        void switch_bank(uint8_t bank);
+        uint8_t* rom_data;
+        size_t rom_size;
+        size_t ram_size;
+        uint8_t* rom_bank0;
+        uint8_t* rom_bankX;
+        uint8_t curr_bank;
+        uint16_t cart_type;
 };
 #endif
